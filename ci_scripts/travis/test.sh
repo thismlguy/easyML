86029f519f7f1d9ffa3b0e1fc7841b81778e2aee
aarshayj <aarshay.jain@gmail.com>
2017-01-16 19:02:18 -0500

    Travis configuration modified | Pytest added in place of nose test | pytest-cov dependency added

--- ci_scripts/travis/test.sh
+++ ci_scripts/travis/test.sh
@@ -7,7 +7,7 @@ mkdir -p $TEST_DIR
 cd $TEST_DIR
 
 if [[ "$COVERAGE" == "true" ]]; then
-    nosetests -s --with-coverage --cover-package=$MODULE $MODULE
+	py.test --cov=$MODULE $MODULE/tests/
 else
-    nosetests -s $MODULE
+	py.test $MODULE/tests/
 fi
